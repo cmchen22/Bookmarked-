38f1eb89cccccff78dc07ba3f8b53ac1807dec77
Anna Gabrielle Carpio <aellecarpio@gmail.com>
2026-04-13 15:27:34 -0700

    fix app.tsx conflict markers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,8 @@ import { DashboardPage } from './pages/DashboardPage'
 import { DiscoveryPage } from './pages/DiscoveryPage'
 import { LandingPage } from './pages/LandingPage'
 import { SocialMediaPage } from './pages/SocialMediaPage'
-<<<<<<< HEAD
 import HomePage from './pages/HomePage'
-=======
 import SignInPage from './pages/SignInPage'
->>>>>>> 0e24db690adc822911f762d6ccee9b7dfcdbe485
 
 function App() {
   return (
