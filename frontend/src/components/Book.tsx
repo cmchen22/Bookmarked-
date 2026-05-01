1307667856ee4b2485dbd1498cb1f8fd0a55c457
Anna Gabrielle Carpio <aellecarpio@gmail.com>
2026-05-01 12:45:28 -0700

    fix bookshelf layout scaling and overflows

--- frontend/src/components/Book.tsx
+++ frontend/src/components/Book.tsx
@@ -4,22 +4,20 @@ type BookProps = {
   width?: number;
 };
 
-const Book = ({ height, color, width=40 }: BookProps) => {
+const Book = ({ height, color, width = 40 }: BookProps) => {
   return (
     <div
       className="book"
       style={{
-       height: `${height}px`,
+        height: `${height}px`,
         width: `${width}px`,
         backgroundColor: color,
       }}
     >
-      <div className="book-shine"></div>
       <div className="book-line top"></div>
       <div className="book-line bottom"></div>
       <div className="book-shine"></div>
     </div>
   );
 };
-
 export default Book;
